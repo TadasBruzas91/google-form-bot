2c5e65e235f9dc91729bde1e5d71f9249faacb0b
Tadas Bružas <tadasbruzas91@gmail.com>
2022-02-06 17:23:24 +0200

    Add radio buttons and checkbox functionality

--- form_filler.py
+++ form_filler.py
@@ -24,6 +24,7 @@ class Form(Scraper):
 
     def fill_form(self, data, addresses, data_order,input_titles_for_test, time, value):
         minutes = self.convert_time_to_minutes(time)
+        form_counter = 1
         if data and addresses:
             for index, entry in enumerate(data[0]["Value"]):
                 data_minutes = data[0]["Age"][index]
@@ -31,26 +32,63 @@ class Form(Scraper):
                 if data_minutes > minutes:
                     print("Finish time")
                     return
-                if data_value != value:
+                if data_value != value or value <= 0:
                     continue
                 self.browser.implicitly_wait(10)
                 self.find_inputs(input_titles_for_test)
+
                 for indx, input in enumerate(self.inputs):
-                    inpt_obj = input.find_element(By.TAG_NAME, "input")
-                    inpt_obj.clear()
+                    entry_data = None
+                    input_type = data_order[indx].split("-")[0].lower()
+
+                    if input_type != "radio" and input_type != "checkbox" and input_type != "dropdown":
+                        inpt_obj = input.find_element(By.TAG_NAME, "input")
+                        inpt_obj.clear()
+                    
+
                     if index + 1 > len(addresses):
                         print("Finish addresses")
                         return 0
-                    if data_order[indx] == "Address":
-                        entry_data = addresses[index]
+
+                    if data_order[indx] == "Address_from_file":
+                        entry_data = addresses[form_counter - 1]
+
+                    elif input_type == "radio":
+                        choise = int(data_order[indx].split("-")[1])
+                        radio_buttons = self.inputs[indx].find_elements(By.CLASS_NAME, "appsMaterialWizToggleRadiogroupElContainer")
+                        if radio_buttons:
+                            radio_buttons[choise-1].click()
+
+                    elif input_type == "checkbox":
+                        choises = data_order[indx].split("-")[1:]
+                        checkboxes = self.inputs[indx].find_elements(By.CLASS_NAME, "docssharedWizToggleLabeledContent")
+                        for choise in choises:
+                            checkboxes[int(choise) - 1].click()
+                    
+                    #TODO: Neveikia dropdown list
+                    # elif input_type == "dropdown":
+                    #     choise = data_order[indx].split("-")[1]
+
+                    #     dropdown = self.inputs[indx].find_element(By.CLASS_NAME, "quantumWizMenuPaperselectOption")
+                    #     if dropdown:
+                    #         dropdown.click()
+                    #         options = self.inputs[indx].find_elements(By.CSS_SELECTOR, ".quantumWizMenuPaperselectContent.exportContent")
+
+                    #         if options:
+                    #             print(options[1].text)
+                        
+
                     else:
                         entry_data = data[0][data_order[indx]][index]
-                    inpt_obj.send_keys(entry_data)
+                    
+                    if entry_data:
+                        inpt_obj.send_keys(entry_data)
 
                 self.browser.implicitly_wait(10)
                 btn = self.browser.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span')
                 btn.click()
-                print(f"Form {index + 1} completed!")
+                print(f"Form {form_counter} completed!")
+                form_counter += 1
 
                 self.browser.implicitly_wait(10)
                 link = self.browser.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
@@ -64,12 +102,12 @@ class Form(Scraper):
             minutes = int(time_list[0])
         elif time_list[1].lower() == "hr" or time_list[1].lower() == "hrs":
             minutes = int(time_list[0]) * 60
-        if len(time_list) > 4:
-            minutes += int(time_list[2])
+            if len(time_list) > 4:
+                minutes += int(time_list[2])
         elif time_list[1].lower() == "day" or time_list[1].lower() == "days":
             minutes = int(time_list[0]) * 1440
-        if len(time_list) > 4:
-            minutes += int(time_list[2]) * 60
+            if len(time_list) > 4:
+                minutes += int(time_list[2]) * 60
         return minutes
 
 
